e85181f5f430f4ec729fcf5e3e6276d345c87f61
shankaraxis <79733453+shankaraxis@users.noreply.github.com>
2023-02-15 07:44:28 +0530

    Add files via upload

---
+++ api/models/food/foodModel.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+
+const foodSchema =  mongoose.Schema({
+    sourceUrl: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    videoUrl: {
+        type: String
+    },
+    videoDuration: {
+        type: String
+    },
+    foodName: {
+        type: String
+    },
+    foodDescription: {
+        type: String
+    },
+    prepTime: {
+        type: String
+    },
+    cookTime: {
+        type: String
+    },
+    totalTime: {
+        type: String
+    },
+    recipeNutrition: [String],
+
+    recipeIngredient: [String],
+    
+    recipeInstructions: [String],
+
+    recipeCuisine: [String],
+    
+    recipeCategory: [String],
+    recipeYield:  { 
+        type: String
+    },
+    authorName: {
+        type: String
+    }
+    
+}, {timestamps:true});
+
+const Food = mongoose.model('Food', foodSchema);
+
+export const foodModel = Food
+export const schemaFood = foodSchema 
